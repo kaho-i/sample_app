07958bd87e36be31b9d5108747afd73a78d16494
EC2 Default User <ec2-user@ip-172-31-11-96.ap-northeast-1.compute.internal>
2024-03-12 10:24:40 +0000

    5-15

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,14 @@ class ListsController < ApplicationController
   end
   
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice]="投稿に成功しました"
+      redirect_to list_path(@list.id)
+    else
+      flash.now[:alert]="投稿に失敗しました"
+      render:new
+    end
   end
 
   def index
@@ -18,9 +23,24 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+  
+  private
   #ストロングパラメータ
-  def list_params
-    params.require(:list).permit(:title,:body)
-  end
+    def list_params
+      params.require(:list).permit(:title,:body,:image)
+    end
 end
